90998a32189e04a0577a9c99b4f5de73c80240ba
Shumax <maxcastro.mv@gmail.com>
2024-02-19 22:29:52 -0300

    feat: add more config table

--- main.tf
+++ main.tf
@@ -16,9 +16,31 @@ resource "aws_dynamodb_table" "tf_notes_table" {
   billing_mode = "PROVISIONED"
   read_capacity = "1"
   write_capacity = "1"
+  hash_key = "noteId"
+  
   attribute {
     name = "noteId"
     type = "S"
   }
-  hash_key = "noteId"
+
+  // Time to live
+  ttl {
+    enabled = true
+    // the attribute name which enforces  TTL, must be a Number      (Timestamp)
+    attribute_name = 7200
+  }
+
+  point_in_time_recovery {
+    enabled = true
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+
 }
+
+# module "table_autoscaling" {
+#   source = "snowplow-devops/dynamodb-autoscaling/aws" // add the autoscaling module
+#   table_name = aws_dynamodb_table.tf_notes_table.name // apply autoscaling for the tf_notes_table
+# }
