111122a39681a253177e9e8ff8cbb4d85ae12801
Shumax <maxcastro.mv@gmail.com>
2024-02-19 22:21:20 -0300

    feat: create table

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ provider "aws" {
   region = "us-east-1"
   shared_credentials_files = ["$HOME/.aws/credentials"]
 }
+
+resource "aws_dynamodb_table" "tf_notes_table" {
+  name = "tf-notes-table"
+  billing_mode = "PROVISIONED"
+  read_capacity = "1"
+  write_capacity = "1"
+  attribute {
+    name = "noteId"
+    type = "S"
+  }
+  hash_key = "noteId"
+}
